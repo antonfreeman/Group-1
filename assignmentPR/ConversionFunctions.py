5b8049076f84d3e0bb50ccf540cea6c6e5b32ee8
Anton Freeman <asfreeman12@gmail.com>
2020-09-08 19:37:27 -0700

    Added some flexibility

--- assignmentPR/ConversionFunctions.py
+++ assignmentPR/ConversionFunctions.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # Python Code Placeholder
 
-test_num = "123.456"
+
+# if we can't use string functionality because of machine constraints switch to list of chars
+test_num = "123.456"  
 
 numer = 0	# the numerator of the number behind the dcm pnt
 denom = 1	# the denominator of the number behind the dcm pnt
@@ -9,14 +11,14 @@ c = 0 		# the characteristic of the number
 
  
 """
-    parse through string and remove 
+    parse through string or char list and remove 
     whole integer from float                                 
 """
 def characteristic( numString, int_c):
     # code here
 
     # steps: feel free to change this is rough
-	# 1. parse string until "."
+	# 1. parse string/char list until "."
 	# 2. grab index[0:position of "."]
 	# 3. assign the result to external variable
 	# 4. Return success or failure flag
@@ -24,14 +26,14 @@ def characteristic( numString, int_c):
 
 
 """
-    parse through string and remove
+    parse through string or char list and remove
     numbers behind decimal point from float
 """
 def mantissa( numString, nom, denom ):
     # code here
 
     # steps: again feel free to change this is rough
-        # 1. parse string until "."
+        # 1. parse string/char list until "."
         # 2. grab index[position of ".": end]
 	# 3. convert decimal to fraction
         # 4. assign the result to external variables
