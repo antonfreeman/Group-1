3e149da4b8e64a40f08800c8929517b6fbd640e7
antonfreeman <asfreeman12@gmail.com>
2020-09-08 19:23:08 -0700

    added template python code

--- assignmentPR/ConversionFunctions.py
+++ assignmentPR/ConversionFunctions.py
@@ -1,1 +1,42 @@
+#!/usr/bin/env python3
 # Python Code Placeholder
+
+test_num = "123.456"
+
+numer = 0	# the numerator of the number behind the dcm pnt
+denom = 1	# the denominator of the number behind the dcm pnt
+c = 0 		# the characteristic of the number
+
+ 
+"""
+    parse through string and remove 
+    whole integer from float                                 
+"""
+def characteristic( numString, int_c):
+    # code here
+
+    # steps: feel free to change this is rough
+	# 1. parse string until "."
+	# 2. grab index[0:position of "."]
+	# 3. assign the result to external variable
+	# 4. Return success or failure flag
+    return False    
+
+
+"""
+    parse through string and remove
+    numbers behind decimal point from float
+"""
+def mantissa( numString, nom, denom ):
+    # code here
+
+    # steps: again feel free to change this is rough
+        # 1. parse string until "."
+        # 2. grab index[position of ".": end]
+	# 3. convert decimal to fraction
+        # 4. assign the result to external variables
+	# 5. save numarator->numer and denominator->denom 
+        # 4. Return success or failure flag
+
+    return False
+
